61eefc1665bfa22b3e2ade85533a6cf00f9e89af
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-11 16:36:30 +0100

    feat: added routerLink property to buttons

--- src/app/features/admin-dashboard/admin-main/admin-main.ts
+++ src/app/features/admin-dashboard/admin-main/admin-main.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ButtonSmall} from '../../../shared/button-small/button-small';
 import {NoteSection} from '../../../layout/note-section/note-section';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-admin-main',
   imports: [
     ButtonSmall,
-    NoteSection
+    NoteSection,
+    RouterLink
   ],
   templateUrl: './admin-main.html',
 })
--- src/app/features/salesman-dashboard/salesman-main/salesman-main.ts
+++ src/app/features/salesman-dashboard/salesman-main/salesman-main.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {NoteSection} from '../../../layout/note-section/note-section';
 import {FilterPill} from '../../../shared/fitler-pill/filter-pill.component';
 import {ButtonSmall} from '../../../shared/button-small/button-small';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-salesman-main',
   imports: [
     NoteSection,
     FilterPill,
-    ButtonSmall
+    ButtonSmall,
+    RouterLink
   ],
   templateUrl: './salesman-main.html',
 })
