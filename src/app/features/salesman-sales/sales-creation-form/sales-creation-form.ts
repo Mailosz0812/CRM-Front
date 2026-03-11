c602fa1eae01474272256771d6e9c08a0289d277
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-11 16:21:17 +0100

    feat: Sale creation form added

---
+++ src/app/features/salesman-sales/sales-creation-form/sales-creation-form.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-creation-form',
+  imports: [],
+  templateUrl: './sales-creation-form.html',
+})
+export class SalesCreationForm {
+
+}
