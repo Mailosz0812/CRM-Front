808f5c68b25737a0dbf3b26483590249623b71d0
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-11 16:26:24 +0100

    redesign: changed print button

--- src/app/features/clients/clients.ts
+++ src/app/features/clients/clients.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import {ButtonSmall} from '../../shared/button-small/button-small';
 import {NoteSection} from '../../layout/note-section/note-section';
+import {ClientMaster} from '../../layout/client-master/client-master';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-clients',
   imports: [
     ButtonSmall,
-    NoteSection
+    NoteSection,
+    ClientMaster,
+    RouterLink
   ],
   templateUrl: './clients.html',
 })
 export class Clients {
-  isSidebarOpen = false;
 
 }
