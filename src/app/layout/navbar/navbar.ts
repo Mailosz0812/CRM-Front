05d2caef4ec91cf0f18870015cd59b85b418c8a5
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-20 15:44:05 +0100

    feat: navbar impl with proper routerLinks based on userStateService

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {UserStateService} from '../../core/user/user-state.service';
+import {User} from '../../core/user/user';
+import {AsyncPipe} from '@angular/common';
+import {ButtonSmall} from '../../shared/button-small/button-small';
+import {AuthService} from '../../core/auth/auth.service';
+import {ROLE_HOME_PAGES} from '../../core/auth/role.routes';
+import {map, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
+    RouterLink,
+    AsyncPipe,
   ],
   templateUrl: './navbar.html',
 })
-export class Navbar {
+export class Navbar{
   isMobileMenuOpen = false;
+  isUserMenuOpen = false;
+  _basePath!: Observable<string>;
+  constructor(public userState: UserStateService,public authService: AuthService) {
+    this._basePath = this.userState._stateSub.pipe(
+      map(user => user ? ROLE_HOME_PAGES[user.role] : '/')
+    );
+  }
 
   onToggleMobileMenu(){
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    this.isUserMenuOpen = false;
   }
 
 }
