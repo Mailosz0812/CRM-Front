b21ad89dfa4e243a903bdf884b9d511fec5d6d70
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-20 21:11:27 +0100

    feat: client details view impl

---
+++ src/app/features/clients/client-details/client-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-details',
+  imports: [],
+  templateUrl: './client-details.html',
+})
+export class ClientDetails {
+}
