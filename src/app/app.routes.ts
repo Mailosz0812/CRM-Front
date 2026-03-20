a844b7b53e9d2a1259cb99ccb38d55e9727d6479
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-20 13:02:46 +0100

    fix: moved common routes of salesman and admin to commonRoutes variable in order to follow DRY rule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,24 @@ import {AdminDashboard} from './features/admin-dashboard/admin-dashboard';
 import {GuestGuard} from './core/auth/guest.guard';
 import {AuthGuard} from './core/auth/auth.guard';
 
+
+const commonRoutes: Routes = [
+  { path: 'clients', loadComponent: () => import("./features/clients/clients").then(c => c.Clients)},
+  { path: 'clients/new', loadComponent: () => import("./features/clients/client-creation-form/client-creation-form").then(c => c.ClientCreationForm)},
+  { path: 'clients/details', loadComponent: () => import("./features/clients/client-details/client-details").then(c => c.ClientDetails)},
+  {
+    path: 'sales/new',
+    loadComponent: () => import("./features/salesman-sales/sales-creation-form/sales-creation-form").then(c => c.SalesCreationForm)
+  },
+  {
+    path: 'sales',
+    loadComponent: () => import("./features/salesman-sales/salesman-sales").then(c => c.SalesmanSales)
+  },
+  {
+    path: 'prices',
+    loadComponent: () => import("./features/price-list/price-list").then(c => c.PriceList)
+  }
+]
 export const routes: Routes = [
   {
     path:'',
@@ -19,43 +37,15 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: '', loadComponent: () => import("./features/admin-dashboard/admin-main/admin-main").then(c => c.AdminMain), pathMatch: 'full'},
-      { path: 'clients', loadComponent: () => import("./features/clients/clients").then(c => c.Clients)},
-      { path: 'clients/new', loadComponent: () => import("./features/clients/client-creation-form/client-creation-form").then(c => c.ClientCreationForm)},
-      { path: 'clients/details', loadComponent: () => import("./features/clients/client-details/client-details").then(c => c.ClientDetails)},
-      {
-        path: 'sales/new',
-        loadComponent: () => import("./features/salesman-sales/sales-creation-form/sales-creation-form").then(c => c.SalesCreationForm)
-      },
-      {
-        path: 'sales',
-        loadComponent: () => import("./features/salesman-sales/salesman-sales").then(c => c.SalesmanSales)
-      },
-      {
-        path: 'prices',
-        loadComponent: () => import("./features/price-list/price-list").then(c => c.PriceList)
-      }
+      ...commonRoutes
     ]
   },
   {
     path: 'salesman', loadComponent: () => import("./features/salesman-dashboard/salesman-dashboard").then(c => c.SalesmanDashboard),
     canActivate: [AuthGuard],
     children: [
-      { path: '', loadComponent: () => import("./features/salesman-dashboard/salesman-main/salesman-main").then(c => c.SalesmanMain)},
-      { path: 'clients', loadComponent: () => import("./features/clients/clients").then(c => c.Clients)},
-      { path: 'clients/new', loadComponent: () => import("./features/clients/client-creation-form/client-creation-form").then(c => c.ClientCreationForm)},
-      { path: 'clients/details', loadComponent: () => import("./features/clients/client-details/client-details").then(c => c.ClientDetails)},
-      {
-        path: 'sales/new',
-        loadComponent: () => import("./features/salesman-sales/sales-creation-form/sales-creation-form").then(c => c.SalesCreationForm)
-      },
-      {
-        path: 'sales',
-        loadComponent: () => import("./features/salesman-sales/salesman-sales").then(c => c.SalesmanSales)
-      },
-      {
-        path: 'prices',
-        loadComponent: () => import("./features/price-list/price-list").then(c => c.PriceList)
-      }
+      { path: '', loadComponent: () => import("./features/salesman-dashboard/salesman-main/salesman-main").then(c => c.SalesmanMain), pathMatch: 'full'},
+      ...commonRoutes
     ]
   },
   {
