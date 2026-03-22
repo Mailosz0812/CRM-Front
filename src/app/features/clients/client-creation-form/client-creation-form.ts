1b8446345ca49d66783b3c08f25d257be8b67828
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-22 16:23:49 +0100

    feat: Stable implementation of client creation form with validation and error handling

--- src/app/features/clients/client-creation-form/client-creation-form.ts
+++ src/app/features/clients/client-creation-form/client-creation-form.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {ClientService} from '../../../core/client/client.service';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ClientCreation} from '../../../core/client/models/client-creation';
 
 @Component({
   selector: 'app-client-creation-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './client-creation-form.html',
 })
 export class ClientCreationForm {
+  isSubmitted = false;
+  errorMsg = signal<string | null>(null);
+  form = new FormGroup({
+    name: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    nipNumber: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    address: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    phone: new FormControl<string>('', {nonNullable: true, validators: [Validators.required]}),
+    mail: new FormControl<string>('', {nonNullable: true, validators: [Validators.email, Validators.required]}),
+    decisionPerson: new FormControl<string>('',{nonNullable: true})
+  });
+  happyMsg = signal<string | null>(null);
+  constructor(private clientService: ClientService) {}
 
+  onSubmit(){
+    this.errorMsg.set(null)
+    this.happyMsg.set(null)
+    this.isSubmitted = true;
+    if(this.form.invalid){
+      return;
+    }
+    const clientData: ClientCreation = this.form.getRawValue();
+    this.clientService.createClient(clientData).subscribe({
+      next: (clientResp)=>{
+        this.happyMsg.set(`Klient ${clientResp.name} dodany pomyślnie`)
+      },
+      error: (err: Error) => {
+        this.isSubmitted = false;
+        this.errorMsg.set(err.message);
+      }
+    });
+  }
 }
