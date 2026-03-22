f931b6a3fc0c6567645748fc942edb5842edd4b3
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-22 16:05:23 +0100

    feat: Global error handling implemented with HttpInterceptor usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,26 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {AuthInterceptor} from './core/auth/auth.interceptor';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {ErrorInterceptor} from './core/error/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
   ]
 };
