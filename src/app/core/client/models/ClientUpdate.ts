6e81008228344721128f336cc44635fe90167133
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-26 17:41:02 +0100

    feat: add update functionality to client details view

--- src/app/core/client/models/ClientUpdate.ts
+++ src/app/core/client/models/ClientUpdate.ts
@@ -1,9 +1,8 @@
-interface ClientResponse {
-  id: string;
+ interface ClientUpdate{
+  clientId: string;
   name: string;
-  nipNumber: string;
   address: string;
   phone: string;
   mail: string;
   decisionPerson: string;
-}
+ }
--- src/app/features/clients/client-details/client-details.ts
+++ src/app/features/clients/client-details/client-details.ts
@@ -1,24 +1,80 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ClientService} from '../../../core/client/client.service';
-import {Observable} from 'rxjs';
+import {catchError, EMPTY, Observable, of, tap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
+import {ButtonSmall} from '../../../shared/button-small/button-small';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-client-details',
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    ButtonSmall,
+    ReactiveFormsModule
   ],
   templateUrl: './client-details.html',
 })
 export class ClientDetails implements OnInit{
   clientId: string | null = null;
-  _clientState!: Observable<ClientCreationResp>;
+  _clientState!: Observable<ClientResponse | null>;
+  editClientMode = false;
+  isSubmitted = false;
+  errorMsg = signal<string | null>(null);
+
+  form = new FormGroup({
+    name: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    address: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    phone: new FormControl<string>('',{nonNullable: true, validators: [Validators.required]}),
+    mail: new FormControl<string>('',{nonNullable: true, validators: [Validators.required,Validators.email]}),
+    decisionPerson: new FormControl<string>('',{nonNullable: true}),
+    }
+  )
+
   constructor(private route: ActivatedRoute,private clientService: ClientService) {}
 
   ngOnInit(): void {
+    this._clientState = this.clientService.clientState;
     this.clientId = this.route.snapshot.paramMap.get('id')
-    this._clientState = this.clientService.getClientDetails(this.clientId!);
+    this.clientService.getClientDetails(this.clientId!).subscribe({
+      next: (client) => {
+        if(client){
+          this.form.patchValue({
+            name: client.name,
+            address: client.address,
+            phone: client.phone,
+            mail: client.mail,
+            decisionPerson: client.decisionPerson
+          });
+        }
+      }
+    })
+  }
+
+  onEditClient(){
+    this.editClientMode = true;
+  }
+
+  onSaveChanges(){
+    this.editClientMode = false;
+  }
+
+  onSubmit(){
+    this.isSubmitted = false;
+    this.errorMsg.set(null);
+    if(this.form.invalid){
+      return;
+    }
+
+    const clientData: ClientUpdate = {
+      ...this.form.getRawValue(),
+      clientId: this.clientId!
+    };
+    this.clientService.updateClientDetails(clientData).subscribe({
+      error: (err: Error) => {
+        this.errorMsg.set(err.message);
+      }
+    })
   }
 
 
