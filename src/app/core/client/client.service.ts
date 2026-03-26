dbbf249b667afa7b518e827bad411cf21c92beba
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-26 17:39:06 +0100

    feat: add get and update methods to client service

--- src/app/core/client/client.service.ts
+++ src/app/core/client/client.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {ClientCreation} from './models/client-creation';
-import {catchError, Observable} from 'rxjs';
+import {BehaviorSubject, catchError, EMPTY, Observable, switchMap, take, tap} from 'rxjs';
 import {ClientShortResp} from './models/client-short-resp';
 import {ClientDashboardInfo} from './models/client-dashboard-info';
+import {Router} from '@angular/router';
+import {UserStateService} from '../user/user-state.service';
 
 
 @Injectable({
@@ -11,10 +13,17 @@ import {ClientDashboardInfo} from './models/client-dashboard-info';
 })
 export class ClientService{
   private baseUrl: string = 'http://localhost:8080/Crm/client';
-  constructor(private client: HttpClient){}
+  private _clientState = new BehaviorSubject<null | ClientResponse>(null);
+  public clientState = this._clientState.asObservable();
+  constructor(private client: HttpClient,private router: Router,private userState: UserStateService){}
 
-  createClient(client: ClientCreation): Observable<ClientCreationResp>{
-    return this.client.post<ClientCreationResp>(this.baseUrl,client);
+  createClient(client: ClientCreation): Observable<ClientResponse>{
+    return this.client.post<ClientResponse>(this.baseUrl,client)
+      .pipe(
+        tap((client) => {
+          this._clientState.next(client);
+        })
+      );
   }
 
   getClientsList(): Observable<ClientShortResp[]>{
@@ -25,8 +34,31 @@ export class ClientService{
     const url = this.baseUrl + '/view/' + id;
     return this.client.get<ClientDashboardInfo>(url);
   }
-  getClientDetails(id: string): Observable<ClientCreationResp>{
+  getClientDetails(id: string): Observable<ClientResponse>{
     const url = this.baseUrl + '/' + id;
-    return this.client.get<ClientCreationResp>(url);
+    return this.client.get<ClientResponse>(url)
+      .pipe(
+        tap((client) => {
+          this._clientState.next(client);
+        }),
+        catchError(err => {
+          return this.userState._basePath.pipe(
+            take(1),
+            tap(value => {
+              this.router.navigate([value, 'clients']);
+            }),
+            switchMap(() => EMPTY)
+          );
+        })
+      );
+  }
+
+  updateClientDetails(client: ClientUpdate): Observable<ClientResponse>{
+    return this.client.put<ClientResponse>(this.baseUrl,client)
+      .pipe(
+        tap((client) => {
+          this._clientState.next(client);
+        })
+      );
   }
 }
